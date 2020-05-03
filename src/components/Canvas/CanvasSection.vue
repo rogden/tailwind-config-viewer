e697d367bb9649ff3ccaf96ec0d06b94af4004c5
Ryan Ogden <ryan@expandtheroom.com>
2020-05-03 10:46:07 -0400

    Decrease heading size

--- src/components/Canvas/CanvasSection.vue
+++ src/components/Canvas/CanvasSection.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="mb-16 max-w-full">
-    <h1 class="mb-1 text-4xl text-gray-800">{{ title }}</h1>
+    <h1 class="mb-2 text-3xl text-gray-800">{{ title }}</h1>
     <div class="bg-white p-6 rounded border-gray-300 border">
       <slot />
     </div>
