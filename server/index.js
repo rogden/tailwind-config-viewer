17c64fdb9e6355883eff16bdc23400ed8a54ac03
Ryan Ogden <ryan@expandtheroom.com>
2020-07-21 19:01:30 -0400

    Add cli config options

--- server/index.js
+++ server/index.js
@@ -2,16 +2,23 @@ const Koa = require('koa')
 const serve = require('koa-static')
 const Router = require('@koa/router')
 const portfinder = require('portfinder')
+const program = require('commander')
 const path = require('path')
 const open = require('open')
 const resolveConfig = require('tailwindcss/resolveConfig')
 
+// Setup CLI options
+program
+  .option('-p, --port <port>', 'Port to run the viewer on', 3000)
+  .option('-o, --open', 'Open the viewer in default browser')
+  .option('-c, --config <path>', 'Path to your Tailwind config file', 'tailwind.config.js')
+  .parse(process.argv)
+
 const app = new Koa()
 const router = new Router()
-const port = 3000
 
 router.get('/config', (ctx) => {
-  const tailwindConfigPath = path.resolve(process.argv[2] || (process.cwd(), 'tailwind.config.js'))
+  const tailwindConfigPath = path.resolve(process.cwd(), program.config)
   // delete the require cache of tailwind config so we can pick up new changes
   delete require.cache[tailwindConfigPath]
   const tailwindConfig = require(tailwindConfigPath)
@@ -24,16 +31,15 @@ app
   .use(router.allowedMethods())
 
 portfinder.getPort({
-  port: 3000
+  port: program.port
 }, (err, port) => {
   if (err) {
     throw (err)
   }
 
   app.listen(port, async () => {
-    console.log(__dirname)
-    console.log('Server Started ∹ http://localhost:' + port.toString())
-    open('http://localhost:' + port.toString())
+    console.log('Server Started ∹ http://localhost:' + program.port.toString())
+    if (program.open) open('http://localhost:' + program.port.toString())
   })
 })
 
