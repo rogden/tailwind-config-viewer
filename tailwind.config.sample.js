39f8733b48deb01eb3982ed52d958ae0b1d0d036
Ryan Ogden <ryan@expandtheroom.com>
2020-08-20 19:09:47 -0400

    Add build:docs script and default tailwind config sample

---
+++ tailwind.config.sample.js
@@ -0,0 +1,13 @@
+/**
+ * This is the default tailwind config file used for
+ * the creation of the doc/demo viewer.
+ */
+module.exports = {
+  theme: {
+    extend: {}
+  },
+  variants: {
+    borderColor: ['responsive', 'hover']
+  },
+  plugins: []
+}
