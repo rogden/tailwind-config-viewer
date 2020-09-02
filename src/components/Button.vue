34a37f62df5356052a66fa37bd47cea5c830a7e6
Ryan Ogden <ryan@expandtheroom.com>
2020-09-02 14:22:38 -0400

    Fix button hover bg in dark mode

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,12 @@
 <template>
   <button
-    class="hover:bg-gray-300 py-2 px-4 text-sm text-gray-800 dark:text-gray-400 border border-gray-400 dark:border-gray-700 focus:outline-none rounded"
+    class="
+      py-2 px-4
+      text-sm text-gray-800 dark:text-gray-400
+      border border-gray-400 dark:border-gray-700
+      hover:bg-gray-300 dark-hover:bg-gray-800
+      focus:outline-none rounded
+    "
     :class="{'bg-gray-300 dark:bg-gray-800': selected}"
     v-on="$listeners"
   >
