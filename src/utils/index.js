cd5f604e1da7e748c99d066977f0cf9335dccc40
Ryan Ogden <ryan@expandtheroom.com>
2020-12-13 12:17:39 -0500

    Update labels to remove Tailwind 2.0 DEFAULT prefix

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,3 @@
-export const removeDefaultSuffix = str => str.replace('-default', '')
+export const removeDefaultSuffix = str => str.replace(/-(default|DEFAULT)/, '')
 
 export const remToPx = rem => parseFloat(rem) * parseFloat(getComputedStyle(document.documentElement).fontSize)
